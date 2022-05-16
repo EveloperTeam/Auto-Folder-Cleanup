beab8837332aa9d3d1b791f2f090ae0d2bf71e6c
Kang Dain <dmelli0505@gmail.com>
2022-05-16 15:36:57 +0900

    주석 추가 및 file_list 구조 변경

--- models/dataprocess.py
+++ models/dataprocess.py
@@ -2,9 +2,11 @@ import csv
 import pandas as pd
 import re
 
+# data[n][0] = 원래 이름
+# data[n][1] = 변환된 이름
 def data_processing(file_list):
     for f_idx in range(len(file_list)):
-        file_list[f_idx] = re.sub(r"[^a-zA-Z0-9 ]", "", file_list[f_idx])
+        file_list[f_idx] = [file_list[f_idx], re.sub(r"[^a-zA-Z0-9 ]", "", file_list[f_idx])]
     
     df = pd.DataFrame(file_list)
     df.to_csv("data.csv", index=False)
