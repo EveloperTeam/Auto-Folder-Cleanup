a964a13846a832ec61229d0547f47b4db55ed42b
Kang Dain <dmelli0505@gmail.com>
2022-05-16 15:37:08 +0900

    ipynb to py

--- models/embedding.py
+++ models/embedding.py
@@ -2,37 +2,37 @@
 # ! pip install nltk
 
 import nltk
-nltk.download("popular")
-
-## Install libraries
-# ! python -m pip install --upgrade pip --quiet
-# ! pip install spacy --quiet
-# ! pip3 install spacy-transformers --quiet
-# ! python -m spacy download en_core_web_sm --quiet
-
 import spacy
+import pandas as pd
+from nltk.cluster import KMeansClusterer
+import numpy as np
+from scipy.spatial import distance_matrix
+import csv
+
 #Load bert model
-nlp = spacy.load("en_core_web_sm")
+
 
 # Utility function for generating sentence embedding from the text
 def get_embeddinngs(text):
     return nlp(text).vector
 
-import pandas as pd
-sen = ['A big ship is anchored near here', 'They are sailing on a ship', 'That is an old boat.', 'Are you ready for your big date?', 'Are you sure?',
-       'Are your grandparents doing well?', 'How is your family', 'I live with my parents.', 'Can you take my photo.', 'Can\'t you open the door?']
-data = pd.DataFrame(sen)
-data.columns = ['text']
-data
+def embedding():
+    nltk.download("popular")
+    nlp = spacy.load("en_core_web_sm")
 
-# Generating sentence embedding from the text
-data['emb'] = data['text'].apply(get_embeddinngs)
-data
+    csv_file = open("./data.csv", "r", encoding='ms932', errors="")
+    sen = ['A big ship is anchored near here', 'They are sailing on a ship', 'That is an old boat.', 'Are you ready for your big date?', 'Are you sure?',
+        'Are your grandparents doing well?', 'How is your family', 'I live with my parents.', 'Can you take my photo.', 'Can\'t you open the door?']
+    data = pd.DataFrame(sen)
+    data.columns = ['text']
+    data
+
+    # Generating sentence embedding from the text
+    data['emb'] = data['text'].apply(get_embeddinngs)
+    data
 
 """# Clustering Sentences"""
 
-from nltk.cluster import KMeansClusterer
-import numpy as np
 
 def clustering_question(data,NUM_CLUSTERS = 15):
 
@@ -54,7 +54,6 @@ def clustering_question(data,NUM_CLUSTERS = 15):
 data, ac = clustering_question(data, 3)
 data
 
-from scipy.spatial import distance_matrix
 def distance_from_centroid(row):
     # type of emb and centroid is different, hence using tolist below
     return distance_matrix([row['emb']], [row['centroid'].tolist()])[0][0]
